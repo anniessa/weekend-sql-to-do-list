98e41c43511c5c4b827f91e8e47d3f9e56a37235
Anniessa Antar <anniessa.antar@gmail.com>
2023-02-04 15:31:51 -0600

    created GET and POST requests in client and router side, started adding some functionality to render

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,5 +3,72 @@ console.log('client.js');
 $(document).ready(function() {
     console.log('jQuery sourced');
 
-    
+    $(document).on('click', '#addButton', addTask);
+
+    getTask();
 });
+
+function addTask() {
+    let taskToSend = {
+        title: $('#addTask').val(),
+        notes: $('#addNotes').val(),
+        date: $('#addDueDate').val(),
+        completed: false,
+    };
+    if (
+        $('#addTitle').val() == "" ||
+        $('#addNotes').val() == "" ||
+        $('#addDueDate').val() == ""
+    ) {
+        alert("All fields required.");
+    } else {
+        $('#addTitle').val(""),
+        $('#addNotes').val(""),
+        $('#addDueDate').val("")
+    };
+    saveTask(taskToSend);
+    
+};
+
+function saveTask(taskToSend) {
+    console.log('in saveTask', taskToSend);
+
+    $.ajax({
+        type: 'POST',
+        url: '/tasks',
+        data: taskToSend
+    })
+    .then(() => {
+        getTask();
+    })
+    .catch((error) => {
+        console.log('POST request for /tasks failed', error);
+        $('body').prepend("<h2>failed POST request</h2>");
+    });
+};
+
+function getTask() {
+    console.log('in getTask');
+    $.ajax({
+        method: 'GET',
+        url: '/tasks'
+    })
+    .then((response) => {
+        console.log('in getTask:', response);
+        render(response);
+    });
+}
+
+
+function render(tasks) {
+
+    for (let task of tasks) {
+        $('#taskList').append(
+      `<ul>
+        <li data-id=${task.id}><button data-completed=${task.completed}> ⚪ </button>
+        ${task.title} ${task.notes} ${task.date}</li>
+        <input class='cancel-btn' type='button' value='❌'>
+      </ul>`
+        )  
+    };
+}
--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -2,9 +2,42 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
-//GET
-
 //POST
+router.post('/', (req,res) => {
+    const newTask = req.body;
+    const queryText = `
+    INSERT INTO "tasks" ("title", "notes", "date", "completed")
+    VALUES ($1, $2, $3, $4);`;
+    const queryParams = [
+        newTask.title,
+        newTask.notes,
+        newTask.date,
+        newTask.completed
+    ]
+    pool.query(queryText, queryParams)
+    .then((result) => {
+        console.log("new task results", result);
+        res.sendStatus(201);
+    })
+    .catch((error) => {
+        console.log(`Error making query ${queryText}`, error);
+        res.sendStatus(500);
+    });
+});
+
+//GET
+router.get('/', (req,res) => {
+    let queryText = `SELECT * FROM "tasks";`;
+    pool.query(queryText)
+    .then((result)=> {
+        res.send((result.rows))
+    })
+    .catch((error) => {
+        console.log(`Error making queryText: ${queryText}`, error);
+        res.sendStatus(500);
+    });
+});
+
 
 //PUT
 
